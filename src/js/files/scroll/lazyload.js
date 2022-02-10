77b23064faad1d8e2e45e66ef2bf390c8dbf70c6
INCREATE AGENCY <increateagency@iMac-INCREATE.local>
2022-02-10 09:48:59 +0200

    09.02

--- src/js/files/scroll/lazyload.js
+++ src/js/files/scroll/lazyload.js
@@ -1,12 +1,15 @@
-// import LazyLoad from "vanilla-lazyload";
+//import LazyLoad from "vanilla-lazyload";
 
 // Работает с объектами с класом ._lazy
 
-const lazyMedia = new LazyLoad({
-	elements_selector: '._lazy',
+new LazyLoad({
+	//elements_selector: '._lazy',
+	container: document.querySelector("#s-team"),
+    thresholds: "200% 0px",
 	class_loaded: '_lazy-loaded',
-	use_native: true
+	//use_native: true
 });
+//console.log(lazyMedia)
 
 // Обновить модуль
 //lazyMedia.update();
